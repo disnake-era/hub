a87c7b80099e36b2b8c4c3d64e5f2832e7855372
elenakrittik <dev.elenakrittik@gmail.com>
2023-07-10 01:08:54 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Label from '@/components/Label'
+
+export default function Home() {
+    return (
+        <Label header="Disnake Extension Hub" subheader="The unofficial official hub for disnake extension developers." />
+    )
+}
---
+++ components/Label.tsx
@@ -0,0 +1,25 @@
+export default function Label({ header, subheader }: { header: string, subheader: string }) {
+    return (
+        <>
+            <div className="label">
+                <div className="label-header">{header}</div>
+                <div className="label-subheader">{subheader}</div>
+            </div>
+            <style>{`
+                .label {
+                    margin-top: 100px;
+                    text-align: center;
+                }
+
+                .label-header {
+                    font-weight: 700;
+                    font-size: 50px;
+                }
+
+                .label-subheader {
+                    font-size: 30px;
+                }
+            `}</style>
+        </>
+    )
+}
