fa3bcf0685fd6e68ee826d299aa8e1decd659a86
lena <77104725+elenakrittik@users.noreply.github.com>
2023-10-21 18:01:24 +0300

    feat: Schedule data dumps. (#2)
    * te
    
    * te
    
    * te
    
    * te
    
    * te
    
    * te
    
    * te
    
    * te
    
    * te
    
    * ba
    
    * ba
    
    * ci: Data dump update.
    
    * ba
    
    * ba
    
    * ci: Data dump update.
    
    * ba
    
    * ba
    
    * ba
    
    * ba
    
    * ba
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ scripts/collect-data.ts
@@ -0,0 +1,85 @@
+import { Octokit } from '@octokit/core';
+import { createActionAuth } from '@octokit/auth-action';
+import { writeFileSync } from 'fs';
+
+const octokit = new Octokit({
+  auth: process.env.PAT1,
+  previews: ["hawkgirl-preview"],
+});
+
+(async () => {
+  const extensions: any = await octokit.graphql(`
+    query {
+      search(query: "topic:disnake-extension", type: REPOSITORY, first: 10) {
+        edges {
+          node {
+            ... on Repository {
+              owner {
+                avatarUrl
+                login
+                url
+              }
+              name
+              dependencyGraphManifests(first: 10, withDependencies: true) {
+                edges {
+                  node {
+                    dependencies(first: 10) {
+                      edges {
+                        node {
+                          packageManager
+                          packageName
+                          requirements
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              descriptionHTML
+              diskUsage
+              homepageUrl
+              primaryLanguage {
+                color
+                name
+              }
+              latestRelease {
+                tagName
+              }
+              licenseInfo {
+                spdxId
+              }
+              stargazerCount
+              updatedAt
+              url
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  let exts: Array<Map<string, any>> = new Array();
+
+  for (const repo_edge of extensions.search.edges) {
+    let repo = repo_edge.node;
+    let map = new Map();
+
+    map.set("owner", repo.owner);
+    map.set("name", repo.name);
+    map.set("descriptionHTML", repo.descriptionHTML);
+    map.set("diskUsage", repo.diskUsage);
+    map.set("homepageUrl", repo.homepageUrl);
+    map.set("primaryLanguage", repo.primaryLanguage);
+    map.set("latestRelease", repo.latestRelease);
+    map.set("licenseInfo", repo.licenseInfo);
+    map.set("stargazerCount", repo.stargazerCount);
+    map.set("updatedAt", repo.updatedAt);
+    map.set("url", repo.url);
+
+    exts.push(Object.fromEntries(map));
+  }
+
+  console.debug(`EXTS = ${exts}`)
+
+  writeFileSync("data.json", JSON.stringify(exts));
+})().then(() => console.log("done"));
